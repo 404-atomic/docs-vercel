06844fa6d711aa1c56c869d1f72c4dee1c261ac8
atomic <404-atomic@gmail.com>
2025-01-14 12:36:51 +0800

    first commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,33 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>文档中心</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/404-atomic/docs-vercel',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/404-atomic/docs-vercel',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '文档中心',
   },
+  primaryHue: {
+    dark: 204,
+    light: 204,
+  },
+  search: {
+    placeholder: '搜索文档...'
+  },
+  feedback: {
+    content: null
+  },
+  editLink: {
+    component: null
+  },
+  gitTimestamp: ({ timestamp }) => {
+    return <>最后更新于: {timestamp.toLocaleDateString('zh-CN')}</>
+  }
 }
 
 export default config
